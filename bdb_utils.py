508a66fe934cfb909bb6fe5f469990067057a816
Michael Stepanovic <mrstepanovic@gmail.com>
2017-10-13 18:23:53 -0700

    improved functions in bdb_utils and added lots of docs to nb

--- bdb_utils.py
+++ bdb_utils.py
@@ -1,33 +1,53 @@
 """
-PUT DOCS HERE
+Functions which interact with the BreweryDB API 
 """
+import json
+import requests
+import pandas as pd
+
+def make_df(arr, col_header):
+	df = pd.DataFrame(arr, columns=col_header)
+	df[['ABV', 'IBU', 'SRM']] = df[['ABV', 'IBU', 'SRM']].apply(pd.to_numeric, errors='ignore')
+	return df
 
 def request_data(request_url, request_params):
-    resp = requests.get(url=request_url, params=request_params)
-    return json.loads(resp.text)
+	resp = requests.get(url=request_url, params=request_params)
+	return json.loads(resp.text)
 
-def download_beer_data(request_url, request_params, count):
-    header = ['Brewery', 'Beer Name', 'Style', 'ABV', 'IBU', 'SRM', 'OG']
-    beers = []
-    data = request_data(request_url, request_params)
-    for page in range(data['numberOfPages']):
-        request_params['p'] = page
-        fresh_data = request_data(request_url, request_params)
-        for i in fresh_data['data']:
-            beer = []
-            try:
-                print("\rDownloading ({} of {})".format(len(beers), count), end="")
-                beer.extend([i['breweries'][0]['name'],
-                            i['nameDisplay'],
-                            i['style']['name'],
-                            i['abv'],
-                            i['ibu'],
-                            i['srmId'],
-                            i['originalGravity']])
-                beers.append(beer)
-            except Exception:
-                pass
-            if len(beers) >= count:
-                return pd.DataFrame(beers, columns=header)
-                break
-    return pd.DataFrame(beers, columns=header) 
+def plot_styles(df, number=10):
+	pd.value_counts(df['Style']).head(n=number).iloc[::-1].plot(kind='barh')
+
+def count_pages(request_url, request_params):
+	data = request_data(request_url, request_params)
+	return list(range(data['numberOfPages']))
+
+def download_data(request_url, request_params, count, rand=False):
+	header = ['Brewery', 'Beer Name', 'Style', 'ABV', 'IBU', 'SRM']
+	rows = []
+	if rand==True:
+		request_params['order'] = 'random'
+	pages = count_pages(request_url, request_params)
+	for page in pages:
+		request_params['p'] = page
+		paginated_data = request_data(request_url, request_params)
+		entries = paginated_data['data']
+		for entry in entries:
+			row = []
+			try:
+				print("\rDownloading ({} of {})".format(len(rows)+1, count), end="")
+				row.extend([entry['breweries'][0]['name'],
+					entry['nameDisplay'],
+					entry['style']['name'],
+					entry['abv'],
+					entry['ibu'],
+					entry['srmId']])
+				if row not in rows:
+					rows.append(row)
+			except Exception:
+				pass
+			if len(rows) == count:
+				df = make_df(rows, header)
+				return df
+				break
+	df = make_df(rows, header)
+	return df
